ad00941811e703b6d351abe878dbcb3b9b3d2629
daveylorenzo7@gmail.com <daveylorenzo7@gmail.com>
2026-04-01 21:14:55 +1300

    fix: backend only app.js

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-﻿
+﻿const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
+});
+
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
